0b0b547d44685b42cb7f8fff250256c6b769edbd
Hans-Martin Duringhof <hmduringhof@gmail.com>
2021-03-24 10:44:52 +0100

    feat: implemented UKF measurement prediction

--- SFND_UKF/src/main.cpp
+++ SFND_UKF/src/main.cpp
@@ -19,19 +19,25 @@ int main() {
   //MatrixXd Xsig_pred = MatrixXd(15, 5);
   //ukf.SigmaPointPrediction(&Xsig_pred);
 
-  VectorXd x_pred = VectorXd(5);
-  MatrixXd P_pred = MatrixXd(5, 5);
-  ukf.PredictMeanAndCovariance(&x_pred, &P_pred);
+  //VectorXd x_pred = VectorXd(5);
+  //MatrixXd P_pred = MatrixXd(5, 5);
+  //ukf.PredictMeanAndCovariance(&x_pred, &P_pred);
+
+  VectorXd z_out = VectorXd(3);
+  MatrixXd S_out = MatrixXd(3, 3);
+  ukf.PredictRadarMeasurement(&z_out, &S_out);
 
   // print result
   // std::cout << "Xsig = " << std::endl << Xsig << std::endl;
   // std::cout << "Xsig_aug = " << std::endl << Xsig_aug << std::endl;
   // std::cout << "Xsig_pred = " << std::endl << Xsig_pred << std::endl;
+  // std::cout << "Predicted state" << std::endl;
+  // std::cout << x_pred << std::endl;
+  // std::cout << "Predicted covariance matrix" << std::endl;
+  // std::cout << P_pred << std::endl;
 
-  std::cout << "Predicted state" << std::endl;
-  std::cout << x_pred << std::endl;
-  std::cout << "Predicted covariance matrix" << std::endl;
-  std::cout << P_pred << std::endl;
+  std::cout << "z_pred: " << std::endl << z_out << std::endl;
+  std::cout << "S: " << std::endl << S_out << std::endl;
 
   return 0;
 }
