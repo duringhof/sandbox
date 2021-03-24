0175a9f5bf100b201d6843533d02cc73c8691d73
Hans-Martin Duringhof <hmduringhof@gmail.com>
2021-03-24 09:43:43 +0100

    feat: UKF impl of sigma point aug and pred

--- SFND_UKF/src/main.cpp
+++ SFND_UKF/src/main.cpp
@@ -9,11 +9,19 @@ int main() {
   // Create a UKF instance
   UKF ukf;
 
-  MatrixXd Xsig = MatrixXd(5, 11);
-  ukf.GenerateSigmaPoints(&Xsig);
+  //MatrixXd Xsig = MatrixXd(5, 11);
+  //ukf.GenerateSigmaPoints(&Xsig);
+
+  //MatrixXd Xsig_aug = MatrixXd(7, 15);
+  //ukf.AugmentedSigmaPoints(&Xsig_aug);
+
+  MatrixXd Xsig_pred = MatrixXd(15, 5);
+  ukf.SigmaPointPrediction(&Xsig_pred);
 
   // print result
-  std::cout << "Xsig = " << std::endl << Xsig << std::endl;
+  // std::cout << "Xsig = " << std::endl << Xsig << std::endl;
+  // std::cout << "Xsig_aug = " << std::endl << Xsig_aug << std::endl;
+  std::cout << "Xsig_pred = " << std::endl << Xsig_pred << std::endl;
 
   return 0;
 }
